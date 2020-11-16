795903ac080107e117117b22ed01d99284465b39
Roman A. Voronkin <roman.voronkin@gmail.com>
2020-11-16 16:45:10 +0300

    Добавлено решение задачи C. PyDEP, 6-1-3

---
+++ tasks/01/C. PyDEP, 6-1-3/task.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    path = Path(__file__).absolute().parent.parent.parent.parent \
+           / 'data' / 'input613.txt'
+    with open(path, 'r', encoding='utf8') as fin:
+        sums = []
+
+        for line in fin:
+            line = line.strip()
+            if line:
+                sums.append(sum(int(item) for item in line.split()))
+
+    print(' '.join(str(value) for value in sums))
