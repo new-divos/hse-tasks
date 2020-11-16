dffd91e29f149eec2d19a96351907395ee1dc394
Roman A. Voronkin <roman.voronkin@gmail.com>
2020-11-16 16:26:46 +0300

    Добавлено решение задачи B. PyDEP, 6-1-2

---
+++ tasks/01/B. PyDEP, 6-1-2/task.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    path = Path(__file__).absolute().parent.parent.parent.parent \
+           / 'data' / 'input612.txt'
+    with open(path, 'r', encoding='utf8') as fin:
+        text = fin.read()
+
+    counter = Counter(text.split())
+    words = [(-count, word) for word, count in counter.items()]
+    words.sort()
+
+    print(' '.join(word for _, word in words))
