255e13ce9bad43d207ad3813f306766dd6521ff3
Roman A. Voronkin <roman.voronkin@gmail.com>
2020-11-29 17:04:54 +0300

    Добавлено решение задачи N. PyDEP, 8-8-1 (требуется исправление)

---
+++ tasks/03/N. PyDEP, 8-8-1/task.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+import lxml  # noqa
+
+
+if __name__ == '__main__':
+    path = Path(__file__).parent.parent.parent.parent \
+        / 'data' / 'maps' / 'mapcity.osm'
+
+    with open(path, 'r', encoding='utf8') as fin:
+        xml = fin.read()
+    soup = BeautifulSoup(xml, 'lxml')
+
+    nodes = {}
+    for node in soup.find_all('node'):
+        nodes[node['id']] = (node['lat'], node['lon'])
+
+    buildings = []
+    for way in soup.find_all('way'):
+
+        refs = []
+        for nd in way('nd'):
+            refs.append(nd['ref'])
+
+        if len(nodes) > 1 and refs[0] == refs[-1]:
+            for tag in way('tag'):
+                if tag['k'] == 'building' and tag['v']:
+                    coords = []
+                    for ref in refs:
+                        coords.append("({}, {})".format(*nodes[ref]))
+
+                    buildings.append(f"{way['id']}\n[{', '.join(coords)}]")
+
+    with open(Path.home() / 'output.txt', 'w', encoding='utf8') as fout:
+        fout.write('\n'.join(buildings))
